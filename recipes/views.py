96ace498f32cea310f43c4663f77d9c66485150a
Fernanda assunção <fernandaassuncaosa@gmail.com>
2024-09-29 18:26:54 -0300

    Movendo urls e views para recipes.

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('Uma linda stringa!')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre!')
+
