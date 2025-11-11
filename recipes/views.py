4a8e26e7676fbe84f0696d02321d55f640f3c01f
Fernanda assunção <fernandaassuncaosa@gmail.com>
2025-11-10 23:04:21 -0300

    Colocando o filtro de is_published

--- recipes/views.py
+++ recipes/views.py
@@ -33,8 +33,9 @@ def search(request):
     search_term = request.GET.get('q')
     if not search_term:
         raise Http404()
-    recipes = Recipe.objects.filter(Q(title__icontains=search_term) |
-                                    Q(description__icontains=search_term)).order_by('-id')
+    recipes = Recipe.objects.filter(Q(Q(title__icontains=search_term) |
+                                    Q(description__icontains=search_term)), is_published=True).\
+        order_by('-id')
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
