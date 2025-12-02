66559d7799b40522c1ab141b4e4abf85f3bffa37
Fernanda assunção <fernandaassuncaosa@gmail.com>
2025-12-02 16:53:59 -0300

    Escrevendo o detalhe de cada comando pra conseguir entender melhor.

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,10 +2,10 @@ import math
 
 
 def make_pagination_range(
-        page_range,
-        qyt_pages,
-        current_page):
-    middle_range = math.ceil(qyt_pages / 2)
+        page_range, # sabe quantas receitas vai existir
+        qyt_pages, # quantidade de paginas a serem mostradas
+        current_page): # pagina que o usuario esta
+    middle_range = math.ceil(qyt_pages / 2) # quantas paginas vao ficar antes e depois da que o usuario esta
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
     start_range_offset = abs(start_range) if start_range < 0 else 0
