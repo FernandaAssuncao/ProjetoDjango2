af33477271ee626150cdf65c8d6ae10c00aa6058
Fernanda assunção <fernandaassuncaosa@gmail.com>
2026-02-21 22:13:47 -0300

    Colocando PER_PAGE para viriaveis de ambiente.

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,14 @@ from django.http import Http404
 from .models import Recipe
 from django.db.models import Q
 from utils.pagination import make_pagination
+import os
+
+PER_PAGE = os.environ.get('PER_PAGE', 6)
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -19,7 +22,7 @@ def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
         is_published=True).order_by('-id'))
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
         'pagination_range': pagination_range,
@@ -42,7 +45,7 @@ def search(request):
     recipes = Recipe.objects.filter(Q(Q(title__icontains=search_term) |
                                     Q(description__icontains=search_term)), is_published=True).\
         order_by('-id')
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
