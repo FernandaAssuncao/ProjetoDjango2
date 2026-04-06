c431271ab4fea33fa868b5826eb8f2bbe2ad4802
Fernanda assunção <fernandaassuncaosa@gmail.com>
2026-04-06 20:43:48 -0300

    Testando a paginação do home.

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,6 +1,7 @@
 from django.urls import reverse, resolve
 from recipes import views
 from .test_recipe_base import RecipeTestBase
+from unittest.mock import patch
 
 
 class RecipeViewsTest(RecipeTestBase):
@@ -25,3 +26,16 @@ class RecipeViewsTest(RecipeTestBase):
         self.make_recipe(is_published=False)
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('No recipes found here!!', response.content.decode('utf-8'))
+
+    @patch('recipes.views.PER_PAGE', new=3)
+    def test_recipe_home_is_pagination(self):
+        for c in range(9):
+            kwargs = {'author_data': {'username': f'u{c}'}, 'slug': f's{c}'}
+            self.make_recipe(**kwargs)
+        response = self.client.get(reverse('recipes:home'))
+        recipes = response.context['recipes']
+        pagination = recipes.paginator
+        self.assertEqual(pagination.num_pages, 3)
+        self.assertEqual(len(pagination.get_page(1)), 3)
+        self.assertEqual(len(pagination.get_page(2)), 3)
+        self.assertEqual(len(pagination.get_page(3)), 3)
